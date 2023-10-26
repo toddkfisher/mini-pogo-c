5957e65ae47260ae808fefb821a2c4e991632906
Todd Fisher <todd.k.fisher@outlook.com>
2023-10-25 22:55:14 -0700

    lex_scan_char().

--- src/lex.c
+++ src/lex.c
@@ -51,6 +51,42 @@ uint32_t g_input_column_n = 1;
 
 bool g_lex_debug_print = false;   // Print lexical units as they are scanned.
 
+// Given to us by God.  A gift from Heaven above.
+char *ASCII[] = {
+  [0x00] = "NUL", [0x01] = "SOH", [0x02] = "STX", [0x03] = "ETX",
+  [0x04] = "EOT", [0x05] = "ENQ", [0x06] = "ACK", [0x07] = "BEL",
+  [0x08] = "BS",  [0x09] = "HT",  [0x0A] = "LF",  [0x0B] = "VT",
+  [0x0C] = "FF",  [0x0D] = "CR",  [0x0E] = "SO",  [0x0F] = "SI",
+  [0x10] = "DLE", [0x11] = "DC1", [0x12] = "DC2", [0x13] = "DC3",
+  [0x14] = "DC4", [0x15] = "NAK", [0x16] = "SYN", [0x17] = "ETB",
+  [0x18] = "CAN", [0x19] = "EM",  [0x1A] = "SUB", [0x1B] = "ESC",
+  [0x1C] = "FS",  [0x1D] = "GS",  [0x1E] = "RS",  [0x1F] = "US",
+  [0x20] = "SPC", [0x21] = "!",   [0x22] = "\"",  [0x23] = "#",
+  [0x24] = "$",   [0x25] = "%",   [0x26] = "&",   [0x27] = "'",
+  [0x28] = "(",   [0x29] = ")",   [0x2A] = "*",   [0x2B] = "+",
+  [0x2C] = ",",   [0x2D] = "-",   [0x2E] = ".",   [0x2F] = "/",
+  [0x30] = "0",   [0x31] = "1",   [0x32] = "2",   [0x33] = "3",
+  [0x34] = "4",   [0x35] = "5",   [0x36] = "6",   [0x37] = "7",
+  [0x38] = "8",   [0x39] = "9",   [0x3A] = ":",   [0x3B] = ";",
+  [0x3C] = "<",   [0x3D] = "=",   [0x3E] = ">",   [0x3F] = "?",
+  [0x40] = "@",   [0x41] = "A",   [0x42] = "B",   [0x43] = "C",
+  [0x44] = "D",   [0x45] = "E",   [0x46] = "F",   [0x47] = "G",
+  [0x48] = "H",   [0x49] = "I",   [0x4A] = "J",   [0x4B] = "K",
+  [0x4C] = "L",   [0x4D] = "M",   [0x4E] = "N",   [0x4F] = "O",
+  [0x50] = "P",   [0x51] = "Q",   [0x52] = "R",   [0x53] = "S",
+  [0x54] = "T",   [0x55] = "U",   [0x56] = "V",   [0x57] = "W",
+  [0x58] = "X",   [0x59] = "Y",   [0x5A] = "Z",   [0x5B] = "[",
+  [0x5C] = "\\",  [0x5D] = "]",   [0x5E] = "^",   [0x5F] = "_",
+  [0x60] = "`",   [0x61] = "a",   [0x62] = "b",   [0x63] = "c",
+  [0x64] = "d",   [0x65] = "e",   [0x66] = "f",   [0x67] = "g",
+  [0x68] = "h",   [0x69] = "i",   [0x6A] = "j",   [0x6B] = "k",
+  [0x6C] = "l",   [0x6D] = "m",   [0x6E] = "n",   [0x6F] = "o",
+  [0x70] = "p",   [0x71] = "q",   [0x72] = "r",   [0x73] = "s",
+  [0x74] = "t",   [0x75] = "u",   [0x76] = "v",   [0x77] = "w",
+  [0x78] = "x",   [0x79] = "y",   [0x7A] = "z",   [0x7B] = "{",
+  [0x7C] = "|",   [0x7D] = "}",   [0x7E] = "~",   [0x7F] = "DEL"
+};
+
 bool lex_is_keyword(uint8_t lex_type)
 {
   return lex_type > LX_KEYWORD_BEGIN && lex_type < LX_KEYWORD_END;
@@ -70,6 +106,9 @@ void lex_print(LEXICAL_UNIT *p_lex)
       break;
     case LX_NUMBER:
       printf(" %d\n", p_lex->l_number);
+      break;
+    case LX_CHAR:
+      printf(" %s\n", ASCII[(uint8_t) p_lex->l_char]);
       break;
     default:
       printf("\n");
@@ -249,6 +288,90 @@ static bool lex_scan_symbol(void)
   return retval;
 }
 
+uint8_t lex_x_digit_value(char digit_char)
+{
+  uint8_t result;
+  digit_char = tolower(digit_char);
+  if (digit_char >= '0' && digit_char <= '9') {
+    result = digit_char - '0';
+  } else {
+    result = 10 + digit_char - 'a';
+  }
+  return result;
+}
+
+
+
+bool lex_scan_char(void)
+{
+  char ch = lex_get_char(false);  // Skip over single quote
+  bool result = false;
+  switch (ch) {
+    case '\\':  // '\x<hexdigit>+' or '\n' or '\t' or '\''
+      ch = lex_get_char(false);  // Skip over backslash.
+      if (EOF == ch) {
+        g_current_lex_unit.l_type = LX_ERROR;
+      } else {
+        g_current_lex_unit.l_type = LX_CHAR;
+        if ('x' != tolower(ch)) {
+          // '\n' or '\t' or '\<anychar but eof>'
+          switch (ch) {
+            case 'n':
+              g_current_lex_unit.l_char = '\n';
+              break;
+            case 't':
+              g_current_lex_unit.l_char = '\t';
+              break;
+            default:
+              g_current_lex_unit.l_char = ch;
+              break;
+          }
+          ch = lex_get_char(false);  // Skip over character following backslash.
+          if ('\'' != ch) {
+            g_current_lex_unit.l_type = LX_ERROR;
+          } else {
+            lex_get_char(false);  // Skip over closing single quote.
+            result = true;
+          }
+        } else {
+          // '\x<hexdigit>+' (wraps around 127).
+          uint8_t n = 0;
+          ch = lex_get_char(false);  // Skip 'x'
+          while (isxdigit(ch)) {
+            n = ((n << 4) + lex_x_digit_value(ch));
+            n &= 0x7f;  // ASCII only bitches.
+            ch = lex_get_char(false);
+          }
+          if ('\'' != ch) {
+            g_current_lex_unit.l_type = LX_ERROR;
+          } else {
+            g_current_lex_unit.l_type = LX_CHAR;
+            g_current_lex_unit.l_char = n;
+            result = true;
+            lex_get_char(false);  // Skip over closing single quote.
+          }
+        }
+      }
+      break;
+    default:  // '<char>'
+      g_current_lex_unit.l_type = LX_CHAR;
+      g_current_lex_unit.l_char = ch;
+      ch = lex_get_char(false);
+      if (EOF == ch) {
+        g_current_lex_unit.l_type = LX_ERROR;
+      } else {
+        if ('\'' != ch) {
+          g_current_lex_unit.l_type = LX_ERROR;
+        } else {
+          lex_get_char(false);  // Skip over single quote.
+          result = true;
+        }
+      }
+      break;
+  }
+  return result;
+}
+
 bool lex_scan(void) {
   char ch;
   bool retval = true;
@@ -262,6 +385,8 @@ bool lex_scan(void) {
     retval = lex_scan_identifier_or_keyword();
   } else if (isdigit(ch)) {
     retval = lex_scan_number();
+  } else if ('\'' == ch) {
+    retval = lex_scan_char();
   } else if (ispunct(ch)) {
     retval = lex_scan_symbol();
   } else {
